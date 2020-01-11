aa15920529d1bd0ebfc786a71cbacb0d7f531eef
oresnet <ngophuc993@gmail.com>
2020-01-10 20:18:22 -0800

    Swift-apps-SlideMenu-B-1.0.0
    - Click icon the menu will apear

---
+++ Swift-apps-slideMenu/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  Swift-apps-slideMenu
+//
+//  Created by Sero on 1/10/20.
+//  Copyright © 2020 oresnet. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+}
